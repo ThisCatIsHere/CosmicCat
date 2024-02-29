be3e24d53ce50bfcc6c546b124e09f14ce28b493
sarahhuth <sarahhuth84@gmail.com>
2024-02-29 20:45:04 +0100

    Projekt erstellt

---
+++ CosmicCat/CosmicCat/CosmicCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicCatApp.swift
+//  CosmicCat
+//
+//  Created by Sarah Huth on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct CosmicCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
